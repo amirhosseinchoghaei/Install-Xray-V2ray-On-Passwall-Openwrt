1c20ed09123abe2821c55d0037dc4757f9ae50e8
AmirHosseinChoghaei <127476407+amirhosseinchoghaei@users.noreply.github.com>
2023-04-18 02:05:38 +0330

    Update owo.sh

--- owo.sh
+++ owo.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
 service passwall stop
 cd /tmp
 wget https://hoverco.net/xray-core_1.8.0-1_mipsel_24kc.ipk
 opkg install xray-core_1.8.0-1_mipsel_24kc.ipk -d ram
 rm xray-core_1.8.0-1_mipsel_24kc.ipk
 service passwall restart
-cd
+cd /root/
+
+echo "level one passed !"
