4541a2f6eb744282127c53e89a755897925e67fb
AmirHosseinChoghaei <127476407+amirhosseinchoghaei@users.noreply.github.com>
2023-04-24 11:28:17 +0330

    Update owo.sh

--- owo.sh
+++ owo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 service passwall stop
 cd /tmp
-wget https://hoverco.net/xray-core_1.8.0-1_mipsel_24kc.ipk
-opkg install xray-core_1.8.0-1_mipsel_24kc.ipk -d ram
-rm xray-core_1.8.0-1_mipsel_24kc.ipk
+wget https://hoverco.net/xray-core_1.8.1-1_mipsel_24kc.ipk
+opkg install xray-core_1.8.1-1_mipsel_24kc.ipk -d ram
+rm xray-core_1.8.1-1_mipsel_24kc.ipk
 service passwall restart
 cd /root/
 
