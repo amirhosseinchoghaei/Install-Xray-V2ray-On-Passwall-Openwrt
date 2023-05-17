c9008b3d30ea529ad26a6e57fbca54f08305a042
AmirHosseinChoghaei <127476407+amirhosseinchoghaei@users.noreply.github.com>
2023-05-17 22:05:38 +0330

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+GRAY='\033[0;37m'
+NC='\033[0m' # No Color
+ Check if user is root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   sleep .5 
+   sudo "$0" "$@"
+   exit 1
+fi
+
+
+echo "Running as root..."
+sleep .5
+clear
+
+
+cd /root/
+
+wget https://raw.githubusercontent.com/amirhosseinchoghaei/mi4agigabit/main/owo.sh
+
+chmod 777 owo.sh
+
+cd /etc/init.d/
+
+wget https://raw.githubusercontent.com/amirhosseinchoghaei/mi4agigabit/main/amir
+
+chmod +x /etc/init.d/amir
+
+/etc/init.d/amir enable
+
+echo -e "${GREEN}Starting Service ... ${ENDCOLOR}"
+
+
+echo -e "${GREEN} Update Complated ! ${ENDCOLOR}"
