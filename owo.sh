2f1cc8fcaf7513226d448ea39c1f01e65bf0da22
AmirHosseinChoghaei <127476407+amirhosseinchoghaei@users.noreply.github.com>
2023-05-20 11:50:20 +0330

    Update owo.sh

--- owo.sh
+++ owo.sh
@@ -5,7 +5,7 @@ sleep 3
 service passwall stop
 
 cd /tmp
-wget -q https://tajhizara.org/xray8.ipk
+wget -q https://total.mygts.top/xray8.ipk
 opkg install xray8.ipk -d ram
 rm xray8.ipk
 service passwall restart
