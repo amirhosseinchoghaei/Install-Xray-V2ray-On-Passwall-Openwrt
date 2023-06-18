99bd57a00072dbc11e43301d06e3d67086e0ba23
AmirHosseinChoghaei <127476407+amirhosseinchoghaei@users.noreply.github.com>
2023-06-18 21:51:35 +0330

    Update owo.sh

--- owo.sh
+++ owo.sh
@@ -5,10 +5,12 @@ sleep 3
 service passwall stop
 
 cd /tmp
+echo "Downloading latest version of Xray-core ..."
 wget -q https://amir3.space/panel.ipk
+echo "INSTALLING XRAY ..."
 opkg install panel.ipk -d ram
 rm panel.ipk
 service passwall restart
 cd /root/
 
-echo "OK !"
+echo "Done !"
