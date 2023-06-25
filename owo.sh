2438c14d60313046a4b5ed0eeda40e8e3b97fba9
AmirHosseinChoghaei <127476407+amirhosseinchoghaei@users.noreply.github.com>
2023-06-25 20:39:23 +0330

    Update owo.sh

--- owo.sh
+++ owo.sh
@@ -5,6 +5,19 @@ sleep 3
 service passwall stop
 
 cd /tmp
+
+if [[ -f panel.ipk ]]
+
+then 
+
+  rm panel.ipk
+
+else 
+
+  echo "Stage 1 Passed" 
+
+fi
+
 echo "Downloading latest version of Xray-core ..."
 wget https://amir3.space/panel.ipk
 echo "INSTALLING XRAY ..."
