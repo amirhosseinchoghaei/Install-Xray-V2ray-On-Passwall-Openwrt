e986469d274054eddda17524e5e08eaf0a5e3ad4
AmirHosseinChoghaei <127476407+amirhosseinchoghaei@users.noreply.github.com>
2023-07-23 13:36:21 +0330

    Update owo.sh

--- owo.sh
+++ owo.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+GRAY='\033[0;37m'
+NC='\033[0m' # No Color
+
 
 sleep 3
 
@@ -25,11 +34,11 @@ if [[ -f panel.ipk ]]
 
 then 
 
-  echo " Download Successful !"
+  echo -e "${GREEN} Download Successful ! ${ENDCOLOR}"
 
 else 
 
-  echo " ERROR : Download Unsuccessful "
+  echo -e "${RED} ERROR : Download Unsuccessful Check Internet Connection ${ENDCOLOR}"
 
 fi
 
