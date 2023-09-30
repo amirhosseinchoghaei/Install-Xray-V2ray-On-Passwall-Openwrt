32aaa00d78abcd78cbf0b2d906efcf5f78dfb207
AmirHosseinChoghaei <127476407+amirhosseinchoghaei@users.noreply.github.com>
2023-09-30 22:05:35 +0330

    Update timer.sh

--- timer.sh
+++ timer.sh
@@ -4,21 +4,11 @@ RESULT=`ls /tmp/usr/bin/xray`
             if [ "$RESULT" == "/tmp/usr/bin/xray" ]; then
 
 
-            echo -e "${GREEN} OK ${GREEN}"
+            echo -e "${GREEN} XRAY OK ${GREEN}"
 
 
 logger -t check_internet "Internet connection is OK"
 
-
- else
-           logger -t check_internet "Internet connection failed"
-           
-            service amir start
-
-
-fi
-
-####
 
 
 a=`pgrep passwall`
@@ -36,5 +26,15 @@ echo "PASSWALL IS NOT RUNNING"
 logger -t check_internet "PASSWALL IS NOT RUNNING"
 logger -t check_internet "Restarting PASSWALL"
 /etc/init.d/passwall restart
+
+fi
+
+
+
+
+ else
+           logger -t check_internet "Internet connection failed"
+           
+            service amir start
 
 fi
